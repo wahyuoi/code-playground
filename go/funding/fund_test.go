8e4413d4fe850472bb775cc93a41bd92eb7de02b
Gede Wahyu <gede.wahyu@airyrooms.com>
2016-10-01 17:44:02 +0700

    Concurrent issue

--- go/funding/fund_test.go
+++ go/funding/fund_test.go
@@ -1,6 +1,10 @@
 package funding
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 func BenchmarkFund(b *testing.B) {
 	fund := NewFund(b.N)
@@ -8,6 +12,40 @@ func BenchmarkFund(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		fund.Withdraw(1)
 	}
+
+	if fund.Balance() != 0 {
+		b.Error("Balance was not zero:", fund.Balance())
+	}
+}
+
+const WORKERS = 10
+
+func BenchmarkWithdraw(b *testing.B) {
+	if b.N < WORKERS {
+		return
+	}
+
+	fund := NewFund(b.N)
+
+	dollarsPerFounder := b.N / WORKERS
+
+	fmt.Println("WORKERS:", WORKERS)
+	fmt.Println("b.N:", b.N)
+	fmt.Println("dollarsPerFounder:", dollarsPerFounder)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < WORKERS; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < dollarsPerFounder; i++ {
+				fund.Withdraw(1)
+			}
+		}()
+	}
+
+	wg.Wait()
 
 	if fund.Balance() != 0 {
 		b.Error("Balance was not zero:", fund.Balance())
