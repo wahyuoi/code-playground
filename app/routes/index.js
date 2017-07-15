a2a6f22b48c0ab6a69648170f5c760bf4331ca43
Gede Wahyu <gedewahyu.ap@gmail.com>
2017-07-15 18:32:17 +0700

    nested routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel() {
     this.replaceWith('rentals');
+  },
+
+  model() {
+    return this.get('store').findAll('rental');
   }
 });
+
--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,7 +1,21 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+import Ember from 'ember'
 
-moduleForAcceptance('Acceptance | list rentals');
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list-rentals');
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
+
 
 test('should show rentals as the home page', function (assert) {
   visit('/');
