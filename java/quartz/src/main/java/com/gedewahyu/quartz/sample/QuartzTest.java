a8cae221d9a94556b44415525612306d7b931d44
Gede Wahyu <gedewahyu.ap@gmail.com>
2018-02-26 20:21:52 +0700

    --wip-- [skip ci]

---
+++ java/quartz/src/main/java/com/gedewahyu/quartz/sample/QuartzTest.java
@@ -0,0 +1,19 @@
+package com.gedewahyu.quartz.sample;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class QuartzTest {
+
+  public static void main(String[] args) {
+    try {
+      Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+      scheduler.start();
+      scheduler.shutdown();
+    } catch (SchedulerException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
